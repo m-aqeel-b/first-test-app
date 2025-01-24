aa3111327cae80c361f0d8268eab018f412f733b
m-aqeel-b <maqeel804@gmail.com>
2025-01-24 20:51:05 +0500

    Displayed bundle deatil at bundleDetail page

--- app/routes/app.bundleProducts.$id.jsx
+++ app/routes/app.bundleProducts.$id.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import db from "../db.server";
+import { Layout, Page, TextField } from "@shopify/polaris";
 
 export async function loader({ params }) {
   const { id } = params;
@@ -23,6 +24,27 @@ const bundleProducts = () => {
   const data = useLoaderData();
   console.log("data from loader", data);
   // console.log("par", params.id);
-  return <h1>hellooooo bundle products= {data.getBundleProducts.name}</h1>;
+  return (
+    <>
+      <Page fullWidth>
+        <Layout>
+          <ui-title-bar
+            title={`Bundle Name: ${data?.getBundleProducts?.name}`}
+          ></ui-title-bar>
+          <Layout.Section>
+            <TextField
+              label="Discount Type"
+              value={data?.getBundleProducts?.discountType}
+            />
+
+            <TextField
+              label="Discount Value"
+              value={data?.getBundleProducts?.discountValue}
+            />
+          </Layout.Section>
+        </Layout>
+      </Page>
+    </>
+  );
 };
 export default bundleProducts;
