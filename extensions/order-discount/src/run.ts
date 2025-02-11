4ea78f72c656119e2680eabec7c36d874f80b5de
m-aqeel-b <maqeel804@gmail.com>
2025-02-11 23:32:02 +0500

    code updted

--- extensions/order-discount/src/run.ts
+++ extensions/order-discount/src/run.ts
@@ -16,17 +16,17 @@ const MIN_UNIQUE_PRODUCT=2;
 const DISCOUNT_PER_PRODUCT=5;
 const MAX_DISCOUNT = 20;
 
-export function run(input: any): FunctionRunResult {
-  const uniqueProducts = input.cart.lines.reduce((productIds: any, line: any)=>{
+export function run(input: RunInput): FunctionRunResult {
+  const uniqueProducts = input.cart.lines.reduce((productIds, line)=>{
     productIds.add((line.merchandise as ProductVariant).product.id);
     return productIds;
   }, new Set<string>());
 
-  if(uniqueProducts.Size < MIN_UNIQUE_PRODUCT){
+  if(uniqueProducts.size < MIN_UNIQUE_PRODUCT){
     return EMPTY_DISCOUNT
   }
   else{
-    const discount = Math.min(uniqueProducts.Size*DISCOUNT_PER_PRODUCT, MAX_DISCOUNT);
+    const discount = Math.min(uniqueProducts.size*DISCOUNT_PER_PRODUCT, MAX_DISCOUNT);
     const uniqueDiscountProducts = discount/DISCOUNT_PER_PRODUCT;
     return{
       discountApplicationStrategy: DiscountApplicationStrategy.First,
