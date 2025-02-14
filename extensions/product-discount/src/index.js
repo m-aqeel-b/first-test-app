bbb75ff0d1e1e6c85b6d1ba69eb6a84c3a76a3c0
m-aqeel-b <maqeel804@gmail.com>
2025-02-14 23:24:25 +0500

    bundel discount issue in index.js run func

---
+++ extensions/product-discount/src/index.js
@@ -0,0 +1,30 @@
+
+import { run } from "@shopify/shopify_function";
+
+async function fetchBundleFromDB() {
+  return {
+    productIds: ["gid://shopify/Product/7596326912207"], 
+    discountValue: 10.0,
+    discountType: "percentage", 
+  };
+}
+
+export default run(async (input) => {
+  const bundleData = await fetchBundleFromDB(); 
+  const discounts = [];
+
+  input.cart.lines.forEach((lineItem) => {
+    if (bundleData.productIds.includes(lineItem.merchandise.id)) {
+      discounts.push({
+        value: bundleData.discountType === "percentage" 
+          ? -(bundleData.discountValue / 100) 
+          : -bundleData.discountValue, 
+        valueType: bundleData.discountType,
+      });
+    }
+  });
+
+  return { discounts };
+
+});
+export * from './run';
