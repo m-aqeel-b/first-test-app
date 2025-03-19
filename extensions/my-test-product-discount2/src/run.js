1a60448a414ff2097d1ba070f16a81f5cce4f8d9
m-aqeel-b <maqeel804@gmail.com>
2025-03-19 23:21:30 +0500

    both fixed and percentage is applying

--- extensions/my-test-product-discount2/src/run.js
+++ extensions/my-test-product-discount2/src/run.js
@@ -3,13 +3,14 @@ import { DiscountApplicationStrategy } from "../generated/api";
 export const run = (input) => {
   const discounts = [];
 
-  const bundleItems = input.cart.lines.filter(item => {
-    return item?.bundleType?.value == 'percentage_discount';
-  })
+  const bundleItems = input.cart.lines.filter((item) => {
+    return item?.bundleType?.value == "percentage_discount";
+  });
 
-  bundleItems.forEach((lineItem) => {
+  if (bundleItems.length > 0) {
+    bundleItems.forEach((lineItem) => {
       const bundleValue = lineItem?.bundleDiscount?.value;
-      console.log("check",bundleValue)
+      console.log("percentage", bundleValue);
       discounts.push({
         targets: [{ productVariant: { id: lineItem.merchandise.id } }],
         value: {
@@ -18,11 +19,32 @@ export const run = (input) => {
           },
         },
       });
-  });
-  console.log("discount value",discounts.length)
+    });
+  }
 
-  return { 
-    discounts,  
+  const bundleItems2 = input.cart.lines.filter((item) => {
+    return item?.bundleType?.value === "fixed_discount";
+  });
+  console.log("get fix", bundleItems2);
+  if (bundleItems2.length > 0) {
+    bundleItems2.forEach((lineItem) => {
+      const bundleValue = lineItem?.bundleDiscount?.value;
+      console.log("fixed", bundleValue);
+      discounts.push({
+        targets: [{ productVariant: { id: lineItem.merchandise.id } }],
+        value: {
+          fixedAmount: {
+            amount: bundleValue,
+          },
+        },
+      });
+    });
+  }
+
+  console.log("discount value1", discounts.length);
+
+  return {
+    discounts,
     discountApplicationStrategy: DiscountApplicationStrategy.All,
   };
 };
